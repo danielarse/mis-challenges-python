4d682ef60b970548d6609ef4018755816cd6f696
Daniel.Arapa <danielarse@gmail.com>
2026-05-17 22:18:00 -0500

    Primer ejercicio

---
+++ challenge1.py
@@ -0,0 +1,7 @@
+#Devuelve la suma de 2 números
+#Crea una función que reciba dos números comoa rgumentos y devuelva su suma
+def addition(a,b) :
+    return a+b
+print(addition(-3, -6))
+print(addition(3, 2))
+print(addition(7, 3))
